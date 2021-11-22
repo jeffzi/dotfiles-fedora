515f8d2528e9d1bf0b2cec7764b0ff4259fad19a
jeffzi <jzinque@gmail.com>
2021-11-22 23:11:21 +0100

    fix dotfiles function

--- .config/fish/functions/dotfiles.fish
+++ .config/fish/functions/dotfiles.fish
@@ -1,4 +1,4 @@
 function dotfiles
     # https://harfangk.github.io/2016/09/18/manage-dotfiles-with-a-git-bare-repository.html
-    git --git-dir=$HOME/.dotfiles.git// --work-tree=$HOME $argv
+    git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $argv
 end
