33b6928592fb7889a0caecbd8e65e93df47a62bc
jeffzi <jzinque@gmail.com>
2022-06-17 15:47:48 +0200

    update qtile config

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -8,9 +8,6 @@ function run {
 }
 
 xrandr --output HDMI-0 --auto --output DP-0 --auto --primary --right-of HDMI-0
-# `xrandr \
-#   --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate left \
-#   --output DP-0 --primary --mode 3840x1600 --pos 1080x0`
 run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 run picom --experimental-backends
 run feh --bg-center ~/.config/wallpapers/astronaut_jellyfish.jpg
