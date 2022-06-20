b6d2c51d3ff076c8622e4ac3c54e9904ea8bdfd9
jeffzi <jzinque@gmail.com>
2022-06-20 19:39:15 +0200

    fix rofi

--- .config/rofi/rofi.sh
+++ .config/rofi/rofi.sh
@@ -1,12 +1,10 @@
 #! /bin/sh
 
 rofi \
-	-modi combi,":rofimoji",top,calc \
+	-modi combi,":rofimoji",calc \
 	-show combi \
 	-combi-modi window,drun \
 	-display-combi   \
-    -display-top   \
-    -display-file-browser-extended פּ- \
 	-display-calc  \
 	-hint-welcome "" \
 	-hint-result "" 
